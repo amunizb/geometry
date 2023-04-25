8d3fc1bb0f043920b0684a8d2e1335ca4bdc061b
Álvaro Muñiz Brea <alvaro.muniz.brea@gmail.com>
2023-04-25 12:04:47 +0100

    minor changes

--- src/test.lean
+++ src/test.lean
@@ -1,1 +1,21 @@
 ---TRYING---
+import topology.metric_space.basic
+import topology.algebra.uniform_group
+import topology.algebra.uniform_mul_action
+import topology.algebra.ring.basic
+import topology.algebra.star
+import topology.algebra.order.field
+import ring_theory.subring.basic
+import group_theory.archimedean
+import algebra.order.group.bounds
+import algebra.periodic
+import topology.instances.int
+import data.real.basic
+
+#check topological_space
+#check ℝ × ℝ 
+def self_prod (α : Type*)(n : ℕ) : Type* :=   
+def ℝⁿ := prod.topological_space (ℝ × ℝ) 
+def prod.topological_space
+
+def psimplex_n : set ℝⁿ := {x ∈ ℝⁿ     }
